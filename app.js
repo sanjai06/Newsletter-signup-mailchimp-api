5e85d24b21d2d6ac207a1bfbfb12b998e0addc00
sanjai kumar n <nsksanjai@gmail.com>
2022-03-03 13:20:24 +0530

    44

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/" ,function(req, res){
+    res.send(__dirname + "/signup.html")
+})
+
 app.listen(3000, function(){
     console.log("server is running on port 3000");
 });
