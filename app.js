6c027197f264a73022a45cc53d81048719cf8188
sanjai kumar n <nsksanjai@gmail.com>
2022-03-22 21:11:19 +0530

    final

--- app.js
+++ app.js
@@ -1,63 +1,55 @@
-// jshint esversion:6
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+// Bodyparser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/" ,function(req, res){
-    res.sendFile(__dirname + "/signup.html")
-});
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post("/", function(req, res){
-    const firstname = req.body.fname;
-    const lastname = req.body.lname;
-    const email = req.body.email;
-    
-    const data ={
-        members: [
-            {
-                email_address: email,
-                status: "subcribed",
-                me_field:{
-                    FNAME: firstname,
-                    LNAME: lastname,
-                }
+// Signup Route
+app.post('/signup', (req, res) => {
+  const { firstName, lastName, email } = req.body;
 
-            }
-        ]
-    }
-  const jsonData = JSON.stringify(data);
+  // Make sure fields are filled
+  if (!firstName || !lastName || !email) {
+    res.redirect('/fail.html');
+    return;
+  }
 
-   const url = "https://us14.api.mailchimp.com/3.0/lists/d54e6429b2";
-   
-   const options ={
-       method:"POST",
-       auth:"sanjai:cc8094653c0225d9c513272376f7357b-us14"
-   }
+  // Construct req data
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
-   const request = https.request(url, options,  function(response){
-        response.on("data", function(data){
-            console.log(JSON.parse(data));
-        })
-    }) 
-    
-    request.write(jsonData);
-    request.end();
-});
+  const postData = JSON.stringify(data);
 
-app.listen(3000, function(){
-    console.log("server is running on port 3000");
-});
+  fetch('https://us14.api.mailchimp.com/3.0/lists/d54e6429b2', {
+    method: 'POST',
+    headers: {
+      Authorization: 'auth cc8094653c0225d9c513272376f7357b-us14'
+    },
+    body: postData
+  })
+    .then(res.statusCode === 200 ?
+      res.redirect('/success.html') :
+      res.redirect('/fail.html'))
+    .catch(err => console.log(err))
+})
 
+const PORT = process.env.PORT || 5000;
 
-// api key 
-// cc8094653c0225d9c513272376f7357b-us14
-
-// list id
-// d54e6429b2
+app.listen(PORT, console.log(`Server started on ${PORT}`));
